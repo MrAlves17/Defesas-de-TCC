377936c75bfdd8495b5c5427a53845a2182cf92a
Vítor Alves Barbosa <38429470+MrAlves17@users.noreply.github.com>
2022-06-29 20:56:42 -0300

    Teste Login #44 e PBI completa #13

--- defesasTCC/database/factory.js
+++ defesasTCC/database/factory.js
@@ -12,10 +12,20 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+const Factory = use('Factory')
+Factory.blueprint('App/Models/User', (aaaa, index, data) => {
+  return {
+    nomeUsuario: data.nomeUsuario,
+    email: data.email,
+    senha: data.senha,
+    matricula: data.matricula,
+    idPerfil: data.idPerfil,
+    ehInterno: data.ehInterno
+  }
+})
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/Perfil', (aaaa, index, data) => {
+  return {
+    nomePerfil: data.nomePerfil
+  }
+})
---
+++ defesasTCC/test/unit/banco-de-dados.spec.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const { test } = use('Test/Suite')('Banco De Dados')
+const Factory = use('Factory')
+const fake_obj = use('@faker-js/faker')
+const faker = fake_obj.faker
+const Database = use('Database')
+const Hash = use('Hash')
+
+test('criação e consulta no BD da tabela Perfil e Usuario', async ({ assert }) => {
+	const pObj = {
+		nomePerfil: faker.word.noun()
+	}
+	const perfil = await Factory.model('App/Models/Perfil').create(pObj)
+	const c1 = await Database.select('nomePerfil').from('Perfil').where('nomePerfil','=', pObj.nomePerfil)
+	await assert.deepEqual([pObj], c1)
+	const uObj = { 
+		nomeUsuario: faker.name.firstName(),
+		email: faker.internet.email(),
+		senha: faker.internet.password(),
+		matricula: faker.random.numeric(8),
+		idPerfil: 1,
+		ehInterno: 0 
+	}
+	const user = await Factory.model('App/Models/User').create(uObj)
+	
+	const c2 = await Database.select('nomeUsuario', 'email', 'senha', 'matricula', 'idPerfil', 'ehInterno').from('Usuario').where('nomeUsuario','=', uObj.nomeUsuario)
+	await assert.isTrue(await Hash.verify(uObj.senha, await c2[0].senha))
+	delete uObj.senha
+	delete c2[0].senha
+	await assert.deepEqual([uObj], c2)
+
+}).timeout(0)
